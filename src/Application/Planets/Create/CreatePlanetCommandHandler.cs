fff986333b6f0b92079a74c0e8e48cdd29684df6
Aleix Trasserra Martinez <aleix@enginydigital.cat>
2023-11-07 18:24:52 +0100

    Add UTs

--- src/Application/Planets/Create/CreatePlanetCommandHandler.cs
+++ src/Application/Planets/Create/CreatePlanetCommandHandler.cs
@@ -1,12 +1,13 @@
 using Domain.Planet;
 using Domain.Primitives;
 using Domain.ValueObjects;
+using Domain.DomainErrors;
 using ErrorOr;
 using MediatR;
 
 namespace Application.Planets.Create;
 
-internal sealed class CreatePlanetCommandHandler : IRequestHandler<CreatePlanetCommand, ErrorOr<Unit>>
+public sealed class CreatePlanetCommandHandler : IRequestHandler<CreatePlanetCommand, ErrorOr<Unit>>
 {
     private readonly IPlanetRepository _planetRepository;
     private readonly IUnitOfWork _unitOfWork;
@@ -19,17 +20,14 @@ internal sealed class CreatePlanetCommandHandler : IRequestHandler<CreatePlanetC
 
     public async Task<ErrorOr<Unit>> Handle(CreatePlanetCommand command, CancellationToken cancellationToken)
     {
-
         if (Orbit.Create(command.OrbitalRadius, command.OrbitalPeriod, command.RotationPeriod) is not Orbit orbit)
         {
-            return Error.Validation("Planet.Orbit", "Planet Orbit Has no valid format");
+            return Errors.Planet.OrbitWithBadFormat;
         }
-
         var planet = new Planet(
             new PlanetId(Guid.NewGuid()),
             command.Name,
             orbit);
-
         await _planetRepository.Add(planet);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
         return Unit.Value;
